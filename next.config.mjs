57150743e3bf26502e79673ee565ae40be28afdb
Ryan Da Costa <ryan.l.dacosta@gmail.com>
2025-03-08 21:34:06 +0530

    asset optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
+import ImageMinimizerPlugin from 'image-minimizer-webpack-plugin';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.optimization.minimizer.push(
+            new ImageMinimizerPlugin({
+                test: /\.(jpe?g|png|svg)$/i,
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.imageminMinify,
+                    options: {
+                        plugins: [
+                            ["jpegtran", { progressive: true }],
+                            ["optipng", { optimizationLevel: 5 }],
+                            [
+                                "svgo",
+                                {
+                                    plugins: [
+                                        {
+                                            name: "preset-default",
+                                        },
+                                    ],
+                                },
+                            ],
+                        ]
+                    },
+                },
+            }),
+        )
+        return config
+    }
+};
 
 export default nextConfig;
